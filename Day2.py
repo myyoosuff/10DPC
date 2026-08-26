445c5268edc0b8b90ba5ee46ac3d64cc486e4731
Mohamed Yoosuff <myyoosuff@gmail.com>
2026-08-26 22:23:58 +0300

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,13 @@
+
+a = 5
+x = "Hello, World!"
+y = ['a', 'b', 5, 6] #list
+z = (1, 2, 3, 4) #tuple
+b = {'name': 'John', 'age': 30, 'city': 'New York'} #dictionary
+k = {1, 2, 3, 4, 5} #set
+print (type(a))
+print (type(x))
+print (type(y))
+print (type(z))
+print (type(b))
+print (type(k))
